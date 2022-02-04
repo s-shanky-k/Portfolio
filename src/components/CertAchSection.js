e2e87c7d2b28826b51bea953e5eec5b2da3660ff
Shankar Kumar S <seshankar20@gmail.com>
2022-02-05 01:46:38 +0530

    Added Certifications and Achievements Sections

---
+++ src/components/CertAchSection.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './CertAchSection.css'
+import ListCard from './ListCard';
+
+function CertAchSection() {
+  return (
+        <div className="certach-container">
+            <div className="certach-achievements" id="achievements">
+                <div className="aboutme-title">
+                    <i className="material-icons-outlined">emoji_events</i>
+                    <h1>Achievements</h1>
+                </div>
+                <ol className="relative mt-5 ml-7 w-10/12 lg:w-9/12">
+                    <ListCard props={{title:"Machine Learning", content:"Amazin certificate received", link:"https://github.com"}}/>
+                    <ListCard props={{title:"Machine Learning", content:"Amazin certificate received", link:"https://github.com"}}/>
+                    <ListCard props={{title:"Machine Learning", content:"Amazin certificate received", link:"https://github.com"}}/>
+                    <ListCard props={{title:"Machine Learning", content:"Amazin certificate received", link:"https://github.com"}}/>
+                    <ListCard props={{title:"Machine Learning", content:"Amazin certificate received", link:"https://github.com"}}/>
+                    <ListCard props={{title:"Machine Learning", content:"Amazin certificate received", link:"https://github.com"}}/>
+                </ol>
+            </div>
+
+            <div className="certach-certifications" id="certifications">
+                <div className="aboutme-title">
+                    <i className="material-icons-outlined">verified_user</i>
+                    <h1>Certifications</h1>
+                </div>
+                <ol className="relative mt-5 ml-7 w-10/12 lg:w-9/12">
+                    <ListCard props={{title:"Machine Learning", content:"Amazin certificate received", link:"https://github.com"}}/>
+                    <ListCard props={{title:"Machine Learning", content:"Amazin certificate received", link:"https://github.com"}}/>
+                    <ListCard props={{title:"Machine Learning", content:"Amazin certificate received", link:"https://github.com"}}/>
+                    <ListCard props={{title:"Machine Learning", content:"Amazin certificate received", link:"https://github.com"}}/>
+                    <ListCard props={{title:"Machine Learning", content:"Amazin certificate received", link:"https://github.com"}}/>
+                </ol>
+            </div>
+        </div>
+  );
+}
+
+export default CertAchSection;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AboutMeSection from '../components/AboutMeSection'
+import CertAchSection from '../components/CertAchSection';
 import ExpEduSection from '../components/ExpEduSection';
 import MusicSection from '../components/MusicSection';
 import PhotoSection from '../components/PhotoSection';
@@ -16,6 +17,7 @@ function Home() {
     <ProjectsSection />
     <PublicationsSection />
     <SkillsSection />
+    <CertAchSection />
     <PhotoSection />
     <MusicSection />
     <ResumeSection />
