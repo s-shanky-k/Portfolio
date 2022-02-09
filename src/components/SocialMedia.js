a942fad78ee4cb744655c25973188f965f33fd5e
Shankar Kumar S <seshankar20@gmail.com>
2022-02-09 15:20:10 +0530

    Added Social Media content

---
+++ src/components/SocialMedia.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {ReactComponent as InstagramLogo} from './assets/instagram.svg'
+import {ReactComponent as LinkedinLogo} from './assets/linkedin.svg'
+import {ReactComponent as GithubLogo} from './assets/github.svg'
+
+function SocialMedia() {
+  return (
+      <div className='flex justify-between items-center w-full h-7'>
+          <a href='https://github.com/s-shanky-k'><div className="w-7 relative"><InstagramLogo/></div></a>
+          <a href='https://github.com/s-shanky-k'><div className="w-7 relative"><LinkedinLogo /></div></a>
+          <a href='https://github.com/s-shanky-k'><div className="w-7 relatve"><GithubLogo /></div></a>
+      </div>
+  );
+}
+
+export default SocialMedia;
