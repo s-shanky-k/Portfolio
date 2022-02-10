4b6452747aff12146521e4fe60bc7842de585dbf
Shankar Kumar S <seshankar20@gmail.com>
2022-02-10 14:46:45 +0530

    Added modal to display photo

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -5,8 +5,10 @@ function PhotoContainer({props}) {
   return (
     <>
     <Fade >
-      <div className='bg-[#800f49] p-1 rounded-md transform transition duration-500 hover:scale-105 shadow-lg hover:shadow-2xl hover:cursor-pointer'>
-          <img className='object-cover !w-full !h-full rounded-md' src={`${process.env.PUBLIC_URL}/assets/images/${props.img}`}></img>
+      <div className='w-full h-full'>
+        <div className='bg-[#800f49] w-full h-full p-1 rounded-md transform transition duration-500 hover:scale-105 shadow-lg hover:shadow-2xl hover:cursor-pointer'>
+            <img className='object-cover !w-full !h-full rounded-md' src={`${process.env.PUBLIC_URL}/assets/images/${props.img}`}></img>
+        </div>
       </div>
     </Fade>
     </>
