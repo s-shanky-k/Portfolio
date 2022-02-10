fd3577f5accbe41322d685991f6badf02db9e21c
Shankar Kumar S <seshankar20@gmail.com>
2022-02-10 22:03:22 +0530

    Added more info for education and experience card

--- src/components/TimelineEducationCard.js
+++ src/components/TimelineEducationCard.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Fade from 'react-reveal/Fade';
 
 function TimelineEducationCard({props}) {
+  const [moreInfo, setMoreInfo] = useState(false);
+  const toggleMoreInfo = () => setMoreInfo(!moreInfo);
   return (
     <>
         <div className='w-28 h-12 mb-2'>
@@ -9,8 +12,30 @@ function TimelineEducationCard({props}) {
         <div className='flex items-baseline'>
             <div className="w-fit text-lg font-semibold text-white">{props.name}</div>
         </div>
-        <h2 className="flex text-base items-center mb-1 font-normal text-white">{props.domain}</h2>
-        <div className="text-sm font-thin leading-none text-white">({props.startDate} - {props.endDate})</div>
+        <h2 className="flex text-base items-center font-normal text-white">{props.domain}</h2>
+        <div className="text-sm font-thin mb-1 text-white">({props.startDate} - {props.endDate})
+          {!moreInfo &&
+            <i className="fas fa-info-circle float-right text-base text-[#d52158] hover:cursor-pointer" onClick={toggleMoreInfo}></i>
+          }
+          {moreInfo &&
+            <i class="fas fa-times-circle float-right text-base text-[#d52158] hover:cursor-pointer" onClick={toggleMoreInfo}></i>
+          }
+        </div>
+
+        {moreInfo &&
+          <Fade top>
+            <div className="mb-3 text-sm font-normal text-white ">
+              <div className='mb-1 space-x-2'>
+                <i className="fas fa-bahai text-xs"></i>
+                <span>Designed and developed an unsupervised machine learning model to identify location-based fraud transactions</span>
+              </div>
+              <div className='mb-1 space-x-2'>
+                <i className="fas fa-bahai text-xs"></i>
+                <span>Used Pyspark to perform distributed data processing, model training and prediction.</span>
+              </div>
+            </div>
+          </Fade>
+        }
     </>
   );
 }
--- src/components/TimelineExperienceCard.js
+++ src/components/TimelineExperienceCard.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Fade from 'react-reveal/Fade';
 
 function TimelineExperienceCard({props}) {
+  const [moreInfo, setMoreInfo] = useState(false);
+  const toggleMoreInfo = () => setMoreInfo(!moreInfo);
+
   return (
     <>
         <div className='w-28 h-12 mb-2'>
@@ -11,10 +15,31 @@ function TimelineExperienceCard({props}) {
             <div className="ml-2 text-sm font-thin leading-none text-white">({props.startDate} - {props.endDate})</div>
         </div>
         <div className="flex text-base items-center mb-1 font-normal text-white">{props.role}</div>
-        <div className="mb-4 text-sm font-normal text-gray-400 space-x-2">
+        <div className="mb-1 text-sm font-normal text-gray-400 space-x-2">
             <i className="fas fa-map-marker-alt"></i>
             <span>{props.location}</span>
+            {!moreInfo &&
+              <i className="fas fa-info-circle float-right text-base text-[#d52158] hover:cursor-pointer" onClick={toggleMoreInfo} ></i>
+            }
+            {moreInfo &&
+              <i class="fas fa-times-circle float-right text-base text-[#d52158] hover:cursor-pointer" onClick={toggleMoreInfo}></i>
+            }
         </div>
+
+        {moreInfo &&
+        <Fade top>
+          <div className="mb-3 text-sm font-normal text-white ">
+            <div className='mb-1 space-x-2'>
+              <i className="fas fa-bahai text-xs"></i>
+              <span>Designed and developed an unsupervised machine learning model to identify location-based fraud transactions</span>
+            </div>
+            <div className='mb-1 space-x-2'>
+              <i className="fas fa-bahai text-xs"></i>
+              <span>Used Pyspark to perform distributed data processing, model training and prediction.</span>
+            </div>
+          </div>
+        </Fade>
+        }
     </>
   );
 }
