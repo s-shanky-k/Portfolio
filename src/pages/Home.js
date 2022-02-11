fef6ac8bfd68c6577464b44117a86005ae4d1487
Shankar Kumar S <seshankar20@gmail.com>
2022-02-12 00:58:22 +0530

    Fixed animation delay on LandingSection

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import LandingSection from "../components/LandingSection";
 import AboutMeSection from "../components/AboutMeSection";
 import CertAchSection from "../components/CertAchSection";
@@ -11,11 +12,31 @@ import PublicationsSection from "../components/PublicationsSection";
 import ResumeSection from "../components/ResumeSection";
 import SkillsSection from "../components/SkillsSection";
 import FooterSection from "../components/FooterSection";
+import Loader from "../components/Loader";
 
 function Home() {
+	const [width, setWidth] = useState(window.innerWidth);
+	const [animationDelay, setAnimationDelay] = useState();
+
+	const handleAnimationDelay = () => {
+		setWidth(window.innerWidth);
+		if (width <= 1024) {
+			setAnimationDelay(400);
+		} else {
+			setAnimationDelay(2000);
+		}
+	};
+
+	useEffect(() => {
+		handleAnimationDelay();
+	}, []);
+
+	window.addEventListener("load", handleAnimationDelay);
+	window.addEventListener("resize", handleAnimationDelay);
+
 	return (
 		<>
-			<LandingSection />
+			<LandingSection animationDelay={animationDelay} />
 			<AboutMeSection />
 			<ExpEduSection />
 			<ProjectsSection />
