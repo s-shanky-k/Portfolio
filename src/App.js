c15f94422f0f07add23cc0952e28c4e16cb42341
Shankar Kumar S <seshankar20@gmail.com>
2022-02-12 00:57:54 +0530

    Added Loader

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbarcopy from "./components/Navbar-copy";
+import { useEffect, useState } from "react";
+import Loader from "./components/Loader";
 
 function App() {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => setLoading(false), 2500);
+	}, []);
+
 	return (
 		<div id="app" className="theme">
-			<BrowserRouter>
-				<Navbarcopy />
-				<Routes>
-					<Route path="/" element={<Home />} />
-				</Routes>
-			</BrowserRouter>
+			{loading ? (
+				<Loader />
+			) : (
+				<BrowserRouter>
+					<Navbarcopy />
+					<Routes>
+						<Route path="/" element={<Home />} />
+					</Routes>
+				</BrowserRouter>
+			)}
 		</div>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,27 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      animation: {
-        'spin-vslow': 'spin 25s linear infinite',
-        'spin-slow': 'spin 15s linear infinite'
-      },
-      colors: {
-        'theme-dark-pink': '#800f49',
-        'theme-medium-pink': '#d52158',
-        'theme-bright-pink': '#ed016a',
-        'theme-very-dark-pink': '#410825',
-        'theme-cyan': '#05d5ba',
-        'theme-orange': '#e6873f',
-        'theme-dark-orange-yellow': '#f8c467',
-        'theme-light-orange-yellow': '#ffd68e',
-        'theme-bg-color': '#121212',
-        'theme-black': '#000000',
-        'theme-secondary-black':'#050505',
-        'theme-primary-font-color': '#DDDDDD',
-      }
-    },
-
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			animation: {
+				"spin-med": "spin 4s linear infinite",
+				"spin-vslow": "spin 25s linear infinite",
+				"spin-slow": "spin 15s linear infinite",
+			},
+			colors: {
+				"theme-dark-pink": "#800f49",
+				"theme-medium-pink": "#d52158",
+				"theme-bright-pink": "#ed016a",
+				"theme-very-dark-pink": "#410825",
+				"theme-cyan": "#05d5ba",
+				"theme-orange": "#e6873f",
+				"theme-dark-orange-yellow": "#f8c467",
+				"theme-light-orange-yellow": "#ffd68e",
+				"theme-bg-color": "#121212",
+				"theme-black": "#000000",
+				"theme-secondary-black": "#050505",
+				"theme-primary-font-color": "#DDDDDD",
+			},
+		},
+	},
+	plugins: [],
+};
