0632d93381bf6d8e515e560c88a69c4c9cf010d7
Shankar Kumar S <seshankar20@gmail.com>
2022-02-18 21:18:13 +0530

    Added more data

--- frontend/src/assets/Data/AboutMeData.js
+++ frontend/src/assets/Data/AboutMeData.js
@@ -1,13 +1,14 @@
 export const AboutMeData = {
 	image: "images/aboutme/pic1.jpeg",
 	content: [
-		`Hello there! My name is Shankar Kumar S, friends call me Shankyyyyy!
+		`Hey there!`,
+		`My name is Shankar Kumar S and friends call me Shankyyyyy!
 		 My curiosity towards the possibilities of what can be done using a computer
-		 led me to take up Computer Science as my undergraduate field of study. Over
-		 the years, I have dipped my feet into various domains such as Machine Learning,
-		 Data Science and Web Development. `,
-		`I have always found the feeling of inventing something novel to be absolutely
-		 exhilerating. This pushed me to work on several research projects.`,
+		 led me to take up Computer Science as my undergraduate field of study.`,
+		`Over the years, I have dipped my feet into various domains such as Machine Learning,
+		 Data Science and Web Development. As a result, my projects and experience are from these different domains.`,
+		`Also, I have always found the feeling of inventing something novel to be absolutely
+		 exhilerating. This has pushed me to contribute to research work as well.`,
 		`This website exists so that I can showcase the various shades that make me who I am today.
 		 Hopefully this will make it easier for you to connect with me.`,
 	],
--- frontend/src/assets/Data/PhotosData.js
+++ frontend/src/assets/Data/PhotosData.js
@@ -1,12 +1,12 @@
 export const PhotosData = [
-	{ img: "images/photography/photo1.jpeg" },
+	{ img: "images/photography/photo1.jpg" },
 	{ img: "images/photography/photo2.jpg" },
-	{ img: "images/photography/photo3.jpeg" },
+	{ img: "images/photography/photo3.jpg" },
 	{ img: "images/photography/photo4.jpg" },
-	{ img: "images/photography/photo5.jpeg" },
-	{ img: "images/photography/photo6.jpeg" },
+	{ img: "images/photography/photo5.jpg" },
+	{ img: "images/photography/photo6.jpg" },
 	{ img: "images/photography/photo7.jpg" },
 	{ img: "images/photography/photo8.jpg" },
 	{ img: "images/photography/photo9.jpg" },
-	{ img: "images/photography/photo10.jpeg" },
+	{ img: "images/photography/photo10.jpg" },
 ];
